00d6fd5c259cafd1d07df5cd64ac896f83be8b6b
Ashish Garg <22ucc026@lnmiit.ac.in>
2024-09-08 18:49:16 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,32 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 const { db } = require('./db/db');
-const {readdirSync} = require('fs')
-const app = express()
+const { readdirSync } = require('fs');
+const app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-//middlewares
-app.use(express.json())
-app.use(cors())
+// Middlewares
+app.use(express.json());
+app.use(cors());
 
-//routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+// Routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start server
 const server = () => {
-    db()
-    app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
-}
+  db();
+  app.listen(PORT, () => {
+    console.log('Listening to port:', PORT);
+  });
+};
 
-server()
+server();
